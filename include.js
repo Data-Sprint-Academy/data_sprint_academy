4a77fec453faffff8dd59bdfea707116b2bbadd4
Zafar Mahmood Waris <bus.zmw@gmail.com>
2025-09-28 12:22:37 +0530

    Update include.js

--- include.js
+++ include.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Load header
-  fetch('/header.html')
+  fetch('header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('header').innerHTML = data;
     });
   
   // Load footer
-  fetch('/footer.html')
+  fetch('footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('footer').innerHTML = data;
